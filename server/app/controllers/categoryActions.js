4024ba39bdedeb431547e8a0f806abf8559af7a6
pjroggy <pierrejeanroggy@gmail.com>
2024-05-25 22:46:54 +0200

    harmonia 6 done

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,3 +1,5 @@
+const tables = require("../../database/tables");
+
 const categories = [
   {
     id: 1,
@@ -11,15 +13,20 @@ const categories = [
 
 // declare the actions
 
-const browse = (req, res) => {
-  if (req.query.q != null) {
-    const filteredCategories = categories.filter((category) =>
-      category.name.includes(req.query.q)
-    );
-    res.json(filteredCategories);
-  } else {
-    res.json(categories);
-  }
+// const browse = (req, res) => {
+//   if (req.query.q != null) {
+//     const filteredCategories = categories.filter((category) =>
+//       category.name.includes(req.query.q)
+//     );
+//     res.json(filteredCategories);
+//   } else {
+//     res.json(categories);
+//   }
+// };
+const browse = async (req, res) => {
+  const categoriesFromDB = await tables.category.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read = (req, res) => {
--- server/app/controllers/programActions.js
+++ server/app/controllers/programActions.js
@@ -1,3 +1,5 @@
+const tables = require("../../database/tables");
+
 // Some data to make the trick
 
 const programs = [
@@ -25,15 +27,20 @@ const programs = [
 
 // Declare the action
 
-const browse = (req, res) => {
-  if (req.query.q != null) {
-    const filteredPrograms = programs.filter((program) =>
-      program.synopsis.includes(req.query.q)
-    );
-    res.json(filteredPrograms);
-  } else {
-    res.json(programs);
-  }
+// const browse = (req, res) => {
+//   if (req.query.q != null) {
+//     const filteredPrograms = programs.filter((program) =>
+//       program.synopsis.includes(req.query.q)
+//     );
+//     res.json(filteredPrograms);
+//   } else {
+//     res.json(programs);
+//   }
+// };
+const browse = async (req, res) => {
+  const programsFromDB = await tables.program.readAll();
+
+  res.json(programsFromDB);
 };
 
 const read = (req, res) => {
